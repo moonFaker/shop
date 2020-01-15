745d8db04d38d550b5f276d23a0b7bddccdf6936
mrlb <mrlb@163.com>
2020-01-15 23:12:43 +0800

    项目提交

---
+++ src/main/java/com/data/shop/ShopStartApplication.java
@@ -0,0 +1,15 @@
+package com.data.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class ShopStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopStartApplication.class,args);
+    }
+}
